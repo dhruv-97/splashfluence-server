97b9e045f838df353a98df7ab0d498befe0eab0c
Dhruv Mongia <dhruv.mongia@gmail.com>
2017-04-10 01:55:44 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://dhruv:abc@ds137730.mlab.com:37730/confusion',
-    'facebook': {
-        clientID: '432948410371020',
-        clientSecret: 'd7b8565556425f9a6332271abc4a80a4',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
-}
