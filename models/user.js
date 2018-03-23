bbacc19569717790d746b2c41fed853becd73389
Dhruv Mongia <dhruv.mongia@gmail.com>
2018-03-24 01:09:12 +0530

    updating schema

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-    username: { type: String, required: true},
-    email: { type: String, required: true},
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     phone: String,
     resetPasswordToken: String,
